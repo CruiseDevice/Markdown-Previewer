b1409942b05f97f050c6068766d16abdb677bf20
Akash Chavan <meakashchavan@gmail.com>
2017-03-09 18:53:59 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import marked from 'marked'
+
+var initialSource = getDefaultSource();
+
 class App extends Component {
   getMarkDownText(){
     var rawMarkup = marked('# This is _Markdown_.',{sanitize: true});
@@ -8,19 +11,62 @@ class App extends Component {
       __html: rawMarkup
     };
   }
+  onChange(e){
+    this.setState({
+      source:e.target.value
+    });
+    this.storeSource(e.target.value);
+  }
   render() {
     return (
       <div className="App">
-        <div className="heading">
-          <h1>MarkDown Previewer</h1>
+        <div>
+          <textarea
+            className = "editor"
+            defaultValue = {initialSource}
+            onChange = {this.onChange}>
+          </textarea>
+          <div className = "preview" dangerouslySetInnerHTML={this.getMarkDownText()}/>
         </div>
-        <textarea
-          className = "editor">
-        </textarea>
-        <div dangerouslySetInnerHTML={this.getMarkDownText()}/>
       </div>
     );
   }
 }
-
+function getDefaultSource(){
+  return [
+    'Heading','',
+    '=======',
+    '',
+    'Sub-heading',
+    '-----------',
+    '',
+    '### Another deeper heading',
+    '',
+    'Paragraphs are separated',
+    'by a blank line.',
+    '',
+    'Leave 2 spaces at the end of a line to do a',
+    'line break',
+    '',
+    'Text attributes *italic*, **bold**,',
+    '`monospace`, ~~strikethrough~~ .',
+    '',
+    'Shopping list:',
+    '',
+    ' * apples',
+    ' * oranges',
+    ' * pears',
+    '',
+    'Numbered list:',
+    '',
+    ' 1. apples',
+    ' 2. oranges',
+    ' 3. pears',
+    '',
+    'The rain---not the reign---in',
+    'Spain.',
+    '',
+    '*[Herman Fassett](https://freecodecamp.com/hermanfassett)*'
+  ].join('\n')
+}
 export default App;
