412776966bfd9a95665e2567fee4142c754840a9
CruiseDevice <achavan1211@gmail.com>
2025-04-10 16:06:31 -0700

    Migrating code to TypeScript

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+// Simple test using React 18's createRoot API
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  const root = createRoot(div);
+  root.render(<App />);
+  root.unmount();
+});
